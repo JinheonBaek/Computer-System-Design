d0dc5c0d1185e59581d7e4f2364b65bae78b48a9
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-04-07 23:13:52 +0900

    first commit of assignment #4, just init assignment #4 env

---
+++ Assignment-4/class_experiments.sdk/csd_lab/Debug/src/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+S_SRCS += \
+../src/uart_init.s 
+
+S_UPPER_SRCS += \
+../src/uart_polling.S 
+
+OBJS += \
+./src/uart_init.o \
+./src/uart_polling.o 
+
+S_UPPER_DEPS += \
+./src/uart_polling.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 gcc assembler'
+	arm-none-eabi-gcc -c  -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 gcc compiler'
+	arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I../../csd_lab_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Assignment-4/class_experiments.sdk/csd_lab/Debug/src/uart_polling.d
@@ -0,0 +1,4 @@
+src/uart_polling.o src/uart_polling.o: ../src/uart_polling.S \
+ ../src/uart_regs.h
+
+../src/uart_regs.h:
