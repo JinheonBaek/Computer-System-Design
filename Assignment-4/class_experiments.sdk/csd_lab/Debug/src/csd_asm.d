a0fe2fa7b5336e30b5fb704a98533277ecc1f810
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-04-08 12:57:49 +0900

    if type any char, return & line feed & add null end of string

--- Assignment-4/class_experiments.sdk/csd_lab/Debug/src/csd_asm.d
+++ Assignment-4/class_experiments.sdk/csd_lab/Debug/src/csd_asm.d
@@ -1,3 +1,6 @@
-src/csd_asm.o src/csd_asm.o: ../src/csd_asm.S ../src/uart_regs.h
+src/csd_asm.o src/csd_asm.o: ../src/csd_asm.S ../src/uart_regs.h \
+ ../src/uart_init.s
 
 ../src/uart_regs.h:
+
+../src/uart_init.s:
--- Assignment-4/class_experiments.sdk/csd_lab/Debug/src/subdir.mk
+++ Assignment-4/class_experiments.sdk/csd_lab/Debug/src/subdir.mk
@@ -6,6 +6,9 @@
 LD_SRCS += \
 ../src/lscript.ld 
 
+S_SRCS += \
+../src/uart_init.s 
+
 C_SRCS += \
 ../src/csd_main.c \
 ../src/csd_print.c 
@@ -16,7 +19,8 @@ S_UPPER_SRCS += \
 OBJS += \
 ./src/csd_asm.o \
 ./src/csd_main.o \
-./src/csd_print.o 
+./src/csd_print.o \
+./src/uart_init.o 
 
 S_UPPER_DEPS += \
 ./src/csd_asm.d 
@@ -38,6 +42,13 @@ src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM v7 gcc compiler'
 	arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I../../csd_lab_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 gcc assembler'
+	arm-none-eabi-gcc -c  -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
