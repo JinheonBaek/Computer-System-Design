dc9ef326cca9352403b547360db72da9e862979c
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-05-27 21:48:29 +0900

    Assignment #8

---
+++ Assignment-8/class_experiments.sdk/csd_lab/Debug/src/csd_asm.d
@@ -0,0 +1,4 @@
+src/csd_asm.o src/csd_asm.o: ../src/csd_asm.S \
+ ../src/csd_zynq_peripherals.h
+
+../src/csd_zynq_peripherals.h:
---
+++ lab_caches/csd_main.c
@@ -0,0 +1,27 @@
+/*
+ * csd_main.c
+ *
+ *  Created on: 2018. 4. 30.
+ *      Author: Taeweon Suh
+ */
+
+unsigned volatile char * gpio_led = (unsigned char *) 0x41200000;
+
+int csd_main()
+{
+
+ int count;
+
+ while (1) {
+
+	for (count=0; count < 0x400000; count++) ;
+
+	*gpio_led = 0xC3;
+
+	for (count=0; count < 0x400000; count++) ;
+
+	*gpio_led = 0x3C;
+
+ }
+	return 0;
+}
